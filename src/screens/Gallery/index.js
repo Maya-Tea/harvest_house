20b9e9404dea3bd9282997ac626c25680a57c4c6
Mayat Smolnik <mcsmolnik@gmail.com>
2019-06-30 18:25:41 -0500

    yarn add

--- src/screens/Gallery/index.js
+++ src/screens/Gallery/index.js
@@ -35,7 +35,89 @@ export default class WhatWeDo extends Component {
     }
 
     callApi = async() => {
-        
+
+        //https://www.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=1c39a5f195ce4f47540160617dcc3870&user_id=182398770%40N08&format=json&nojsoncallback=1
+
+        //https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=1c39a5f195ce4f47540160617dcc3870&photoset_id=72157709344355577&user_id=182398770%40N08&format=json&nojsoncallback=1
+        const headers = {
+            "Accept": "application/json, application/xml, text/plain, text/html, *.*",
+            "Content-Type": "application/json"
+        };
+        let res = await fetch("https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=1c39a5f195ce4f47540160617dcc3870&photoset_id=72157709344355577&user_id=182398770%40N08&format=json&nojsoncallback=1", {
+            method: "get",
+            headers: headers,
+            //body: JSON.stringify(params)
+        });
+        res = await res.json();
+        const photos= [];
+        res.photoset.photo.map((photo, i) => {
+            photos.push(`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`)
+        })
+        console.log(photos);
+        // "id": "48157279157",
+        // "secret": "df75bb5fef",
+        // "server": "65535",
+        // "farm": 66,
+        // "title": "10440904_10153155144562562_5726708217498249574_n",
+        // "isprimary": "0",
+        // "ispublic": 1,
+        // "isfriend": 0,
+        // "isfamily": 0
+        // }
+
+        //console.log(res);
+        //{
+        // "photoset": {
+        // "id": "72157709328334536",
+        // "primary": "48157280477",
+        // "owner": "182383295@N08",
+        // "ownername": "mcsmolnik",
+        // "photo": [
+        // {
+        // "id": "48157199461",
+        // "secret": "4562908771",
+        // "server": "65535",
+        // "farm": 66,
+        // "title": "apo1",
+        // "isprimary": "0",
+        // "ispublic": 1,
+        // "isfriend": 0,
+        // "isfamily": 0
+        // },
+        // {
+        // "id": "48157280477",
+        // "secret": "abf4495a3c",
+        // "server": "65535",
+        // "farm": 66,
+        // "title": "11060869_10153155144792562_4320474641000498688_n",
+        // "isprimary": "1",
+        // "ispublic": 1,
+        // "isfriend": 0,
+        // "isfamily": 0
+        // },
+        // {
+        // "id": "48157279157",
+        // "secret": "df75bb5fef",
+        // "server": "65535",
+        // "farm": 66,
+        // "title": "10440904_10153155144562562_5726708217498249574_n",
+        // "isprimary": "0",
+        // "ispublic": 1,
+        // "isfriend": 0,
+        // "isfamily": 0
+        // }
+        // ],
+        // "page": 1,
+        // "per_page": 500,
+        // "perpage": 500,
+        // "pages": 1,
+        // "title": "albumMe",
+        // "total": 3
+        // },
+        // "stat": "ok"
+        // }
+        //https://farm66.staticflickr.com/65535/48157279157_df75bb5fef.jpg
+        //https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
     }
 
     render() {
